1baf90c2ac6eec0dfa0ee048ef20ac077a3edfe6
Jisoo Park <qkr1wltn@naver.com>
2023-04-03 17:27:04 +0900

    first commit

---
+++ src/main/java/exercise/springex1/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package exercise.springex1.repository;
+
+import exercise.springex1.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
---
+++ src/test/java/exercise/springex1/SpringEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package exercise.springex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
